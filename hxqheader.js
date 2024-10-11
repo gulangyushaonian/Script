80ed82e58153cce956ecb525fbd2d7502f86d4a7
gulangyushaonian <30430387+gulangyushaonian@users.noreply.github.com>
2024-10-11 22:27:02 +0800

    Update hxqheader.js

--- hxqheader.js
+++ hxqheader.js
@@ -1,7 +1,15 @@
+let body = $response.body; // 获取响应体
 
-let body = $response.body;
+let jsonBody = JSON.parse(body); // 将字符串形式的响应体解析成 JSON 对象
 
-let jsonBody = JSON.parse(body);
-jsonBody.quality = 11;
+// 从 qualities 中提取所有 vtype 值
+let vtypeValues = jsonBody.qualities.map(quality => quality.vtype);
 
-$done({ body: JSON.stringify(jsonBody) });
+// 找到 vtype 的最大值
+let maxVtype = Math.max(...vtypeValues);
+
+// 设置 quality 为 vtype 的最大值
+jsonBody.quality = maxVtype; // 修改 quality 为最大 vtype 值
+
+// 返回修改后的 JSON 对象
+$done({ body: JSON.stringify(jsonBody) }); // 将修改后的 JSON 对象转换成字符串，并返回给客户端
