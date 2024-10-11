c3d6d5984a37b65590d2106580ebe77ea144fc8c
gulangyushaonian <30430387+gulangyushaonian@users.noreply.github.com>
2024-10-11 22:29:57 +0800

    Update hxqheader.js

--- hxqheader.js
+++ hxqheader.js
@@ -8,8 +8,13 @@ let vtypeValues = jsonBody.qualities.map(quality => quality.vtype);
 // 找到 vtype 的最大值
 let maxVtype = Math.max(...vtypeValues);
 
-// 设置 quality 为 vtype 的最大值
-jsonBody.quality = maxVtype; // 修改 quality 为最大 vtype 值
+// 检查 maxVtype 的值是否为 -Infinity （这意味着 qualities 数组可能为空）
+if (maxVtype !== -Infinity) {
+    jsonBody.quality = maxVtype; // 修改 quality 为最大 vtype 值
+} else {
+    // 如果 qualities 为空，保留原来的 quality 值
+    console.log("No qualities available to determine max vtype.");
+}
 
 // 返回修改后的 JSON 对象
 $done({ body: JSON.stringify(jsonBody) }); // 将修改后的 JSON 对象转换成字符串，并返回给客户端
