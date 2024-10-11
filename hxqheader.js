d1df1255d15c49ddc4d3c16e355cfa7a9b059d57
gulangyushaonian <30430387+gulangyushaonian@users.noreply.github.com>
2024-10-12 00:14:44 +0800

    Update hxqheader.js

--- hxqheader.js
+++ hxqheader.js
@@ -1,20 +1,19 @@
-let body = $response.body; // 获取响应体
+let body = $response.body;
 
-let jsonBody = JSON.parse(body); // 将字符串形式的响应体解析成 JSON 对象
+let jsonBody = JSON.parse(body);
 
-// 从 qualities 中提取所有 value 值
 let valueValues = jsonBody.qualities.map(quality => quality.value);
 
-// 找到 value 的最大值
 let maxValue = Math.max(...valueValues);
 
-// 如果 qualities 数组不为空，更新 quality 和 qualityName 字段
 if (valueValues.length > 0) {
-    jsonBody.quality = maxValue; // 修改 quality 为最大 value 值
-    // 找到与最大 value 值对应的质量名称
-    let maxQuality = jsonBody.qualities.find(q => q.value === maxValue);
-    jsonBody.qualityName = maxQuality ? maxQuality.name : ""; // 更新 qualityName
-} 
+    jsonBody.quality = maxValue;
 
-// 返回修改后的 JSON 对象
-$done({ body: JSON.stringify(jsonBody) }); // 将修改后的 JSON 对象转换成字符串，并返回给客户端
+    let maxQuality = jsonBody.qualities.find(q => q.value === maxValue);
+    jsonBody.qualityName = maxQuality ? maxQuality.name : "";
+} else {
+    jsonBody.quality = 10; // 默认值
+    jsonBody.qualityName = "默认质量"; // 可以根据需要设置质量名称
+}
+
+$done({ body: JSON.stringify(jsonBody) });
