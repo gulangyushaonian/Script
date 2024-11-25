da5bacbc92d51ebaa8c5bb869d58cd698ff8e870
gulangyushaonian <30430387+gulangyushaonian@users.noreply.github.com>
2024-11-26 00:33:52 +0800

    Update YiLi.js

--- Script/YiLi.js
+++ Script/YiLi.js
@@ -506,7 +506,7 @@ function recipeTask1() {
       Points: 15,
     });
     body.AuthKey = authKey;
-    body.Method = 'MALLIFCheese.IsHaveAddMemberPoints';
+    body.Method = 'MALLIFCheese.AddMemberPoints1Json';
     body.Params = params;
 
     const opts = {
