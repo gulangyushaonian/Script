98989c9177edadc50911a09f50775dcd0c7ee5f7
gulangyushaonian <30430387+gulangyushaonian@users.noreply.github.com>
2025-05-15 15:01:06 +0800

    Update slzd.js

--- Script/slzd.js
+++ Script/slzd.js
@@ -25,10 +25,10 @@ if (url.indexOf(p1) != -1) {
                 "phone": "",
                 "uid": 2926901,
                 "userVflag": 0,
-                "remark": "此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖!!!作者TG频道 : https://t.me/GieGie777",
+                "remark": "此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖!!!作者TG频道 : 鼓浪屿少年",
                 "pic": "https://zdimg.lifeweek.com.cn/app/20230410/16811146599505136.jpg",
                 "isVip": true,
-                "name": "https://t.me/GieGie777"
+                "name": "鼓浪屿少年"
             }
         },
         "resultMsg": "操作成功！",
@@ -54,21 +54,21 @@ if (url.indexOf(p2) != -1) {
             "vipLevel": 4,
             "vipExpiration": "2999-09-09",
             "vip": true,
-            "username": "https://t.me/GieGie777",
+            "username": "鼓浪屿少年",
             "vipValid": true
         }, {
             "userHeadPic": "https://zdimg.lifeweek.com.cn/app/20230410/16810960185662892.jpg",
             "vipLevel": 5,
             "vipExpiration": "",
             "vip": false,
-            "username": "https://t.me/GieGie777",
+            "username": "鼓浪屿少年",
             "vipValid": true
         }, {
             "userHeadPic": "https://zdimg.lifeweek.com.cn/app/20230410/16810960185662892.jpg",
             "vipLevel": 6,
             "vipExpiration": "2999-09-09",
             "vip": true,
-            "username": "https://t.me/GieGie777",
+            "username": "鼓浪屿少年",
             "vipValid": true
         }],
         "resultMsg": "操作成功！",
