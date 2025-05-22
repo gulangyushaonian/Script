bb7ff5cc0a5e652e88ec6547cd08bc7603e422b8
gulangyushaonian <30430387+gulangyushaonian@users.noreply.github.com>
2025-05-22 09:20:27 +0800

    Create amdc.js

---
+++ Script/amdc.js
@@ -0,0 +1,12 @@
+/***********************************************
+> 应用名称：墨鱼自用amdc脚本
+> 脚本作者：@ddgksf2013
+***********************************************/
+
+
+
+const version = 'V1.0.10';
+
+
+
+var ua=$request.headers["User-Agent"]||$request.headers["user-agent"];/(AMap|Cainiao|%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C|%E5%96%B5%E8%A1%97|%E5%A4%A9%E7%8C%AB|Alibaba|Hema4iPhone|Moon|DMPortal)/.test(ua)?$done({body:"gulangyuboy"}):$done({});
